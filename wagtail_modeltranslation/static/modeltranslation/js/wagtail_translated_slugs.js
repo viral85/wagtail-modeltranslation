5a17e957c2441ca60a8fe6d8138b9e1dfe3b88cb
Ilya Grishkov <me@ilyagrishkov.com>
2016-09-30 11:57:49 +0200

    Use wagtail's urlify for slug generation

--- wagtail_modeltranslation/static/modeltranslation/js/wagtail_translated_slugs.js
+++ wagtail_modeltranslation/static/modeltranslation/js/wagtail_translated_slugs.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
         });
 
         $('#id_title_'+lang_code).on('keyup keydown keypress blur', function() {
-            if ($('body').hasClass('create') || (!$('#id_slug_'+lang_code).data('previous-val').length || cleanForSlug($('#id_title_'+lang_code).data('previous-val')) === $('#id_slug_'+lang_code).data('previous-val'))) {
+            if ($('body').hasClass('create') || (!$('#id_slug_'+lang_code).data('previous-val').length || cleanForSlug($('#id_title_'+lang_code).data('previous-val'), true) === $('#id_slug_'+lang_code).data('previous-val'))) {
                 // only update slug if the page is being created from scratch, if slug is completely blank, or if title and slug prior to typing were identical
-                $('#id_slug_'+lang_code).val(cleanForSlug($('#id_title_'+lang_code).val()));
+                $('#id_slug_'+lang_code).val(cleanForSlug($('#id_title_'+lang_code).val(), true));
             }
         });
     });
